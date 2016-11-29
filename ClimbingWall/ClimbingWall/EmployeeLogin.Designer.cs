16966e7a94f9cb30975b2fa1685c67a10d6386fc
Kenny Grovenburg <Kenny Grovenburg@KENNY-HP>
2016-11-28 20:15:17 -0600

    Set the ShowIcon setting for icon image on EmployeeLogin Form to False [minor change]

--- ClimbingWall/ClimbingWall/EmployeeLogin.Designer.cs
+++ ClimbingWall/ClimbingWall/EmployeeLogin.Designer.cs
@@ -28,75 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.usernameInput = new System.Windows.Forms.TextBox();
-            this.passwordInput = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.loginButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // usernameInput
-            // 
-            this.usernameInput.Location = new System.Drawing.Point(151, 74);
-            this.usernameInput.MaxLength = 255;
-            this.usernameInput.Name = "usernameInput";
-            this.usernameInput.Size = new System.Drawing.Size(220, 20);
-            this.usernameInput.TabIndex = 0;
-            this.usernameInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.usernameInput_KeyDown);
-            // 
-            // passwordInput
-            // 
-            this.passwordInput.Location = new System.Drawing.Point(151, 114);
-            this.passwordInput.MaxLength = 255;
-            this.passwordInput.Name = "passwordInput";
-            this.passwordInput.PasswordChar = '*';
-            this.passwordInput.Size = new System.Drawing.Size(220, 20);
-            this.passwordInput.TabIndex = 1;
-            this.passwordInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordInput_KeyDown);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 77);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Username";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(90, 117);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Password";
-            // 
-            // loginButton
-            // 
-            this.loginButton.Location = new System.Drawing.Point(296, 163);
-            this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(75, 23);
-            this.loginButton.TabIndex = 4;
-            this.loginButton.Text = "Login";
-            this.loginButton.UseVisualStyleBackColor = true;
-            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
-            // 
-            // EmployeeLogin
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 280);
-            this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.passwordInput);
-            this.Controls.Add(this.usernameInput);
-            this.Name = "EmployeeLogin";
-            this.Text = "Employee Login";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.usernameInput = new System.Windows.Forms.TextBox();
+			this.passwordInput = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.loginButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// usernameInput
+			// 
+			this.usernameInput.Location = new System.Drawing.Point(151, 74);
+			this.usernameInput.MaxLength = 255;
+			this.usernameInput.Name = "usernameInput";
+			this.usernameInput.Size = new System.Drawing.Size(220, 20);
+			this.usernameInput.TabIndex = 0;
+			this.usernameInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.usernameInput_KeyDown);
+			// 
+			// passwordInput
+			// 
+			this.passwordInput.Location = new System.Drawing.Point(151, 114);
+			this.passwordInput.MaxLength = 255;
+			this.passwordInput.Name = "passwordInput";
+			this.passwordInput.PasswordChar = '*';
+			this.passwordInput.Size = new System.Drawing.Size(220, 20);
+			this.passwordInput.TabIndex = 1;
+			this.passwordInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordInput_KeyDown);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(90, 77);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Username";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(90, 117);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Password";
+			// 
+			// loginButton
+			// 
+			this.loginButton.Location = new System.Drawing.Point(296, 163);
+			this.loginButton.Name = "loginButton";
+			this.loginButton.Size = new System.Drawing.Size(75, 23);
+			this.loginButton.TabIndex = 4;
+			this.loginButton.Text = "Login";
+			this.loginButton.UseVisualStyleBackColor = true;
+			this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+			// 
+			// EmployeeLogin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(494, 280);
+			this.Controls.Add(this.loginButton);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.passwordInput);
+			this.Controls.Add(this.usernameInput);
+			this.Name = "EmployeeLogin";
+			this.ShowIcon = false;
+			this.Text = "Employee Login";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
