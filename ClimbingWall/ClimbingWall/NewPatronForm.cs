09bffbca2c39874923388c292fc2a276aa1ebc98
SamKool <sam.kool@jacks.sdstate.edu>
2017-03-30 01:36:31 -0500

    Added Input Validation to Patron Creation.

--- ClimbingWall/ClimbingWall/NewPatronForm.cs
+++ ClimbingWall/ClimbingWall/NewPatronForm.cs
@@ -19,19 +19,55 @@ namespace ClimbingWall
 
 		private void CreatePatronButton_Click(object sender, EventArgs e)
 		{
-			if (termsRadio.Checked) {
-				//createPatron(string fName, string lName, string midI, string phone, string email, string pat_Id)
-				bool success = Database.Instance.createPatron(FirstNameTextbox.Text, LastNameTextbox.Text, MiddleInitTextbox.Text, PhoneTextbox.Text, EmailTextbox.Text,  Convert.ToInt32(IDTextbox.Text));
-				if (success) {
-					MessageBox.Show("User creation succeeded.");
-					this.Hide();
+			if (FirstNameTextbox.Text != "" && LastNameTextbox.Text != "" && MiddleInitTextbox.Text != "" && EmailTextbox.Text != "" && PhoneTextbox.Text != "" && IDTextbox.Text != "") {
+				//First make sure all the text boxes are filled
+				bool isValid = true;
+
+				if (!DatabaseInputValidation.nameIsValid(FirstNameTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("First Name has Invalid Input!");
 				}
-				else {
-					MessageBox.Show("User creaton failed.");
+				if (!DatabaseInputValidation.nameIsValid(FirstNameTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("Last Name has Invalid Input!");
+				}
+				if (!DatabaseInputValidation.nameIsValid(FirstNameTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("Middle Initial has Invalid Input!");
+				}
+				if (!DatabaseInputValidation.uintIsValid(PhoneTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("Phone Number has Invalid Input!");
+				}
+				if (!DatabaseInputValidation.uintIsValid(IDTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("First Name has Invalid Input!");
+				}
+				if (!DatabaseInputValidation.stringIsValid(EmailTextbox.Text) && isValid) {
+					isValid = false;
+					MessageBox.Show("First Name has Invalid Input!");
+				}
+
+
+				if (isValid) {
+					if (termsRadio.Checked) {
+						//createPatron(string fName, string lName, string midI, string phone, string email, string pat_Id)
+						bool success = Database.Instance.createPatron(FirstNameTextbox.Text, LastNameTextbox.Text, MiddleInitTextbox.Text, PhoneTextbox.Text, EmailTextbox.Text, Convert.ToInt32(IDTextbox.Text));
+						if (success) {
+							MessageBox.Show("User creation succeeded.");
+							this.Hide();
+						}
+						else {
+							MessageBox.Show("User creaton failed.");
+						}
+					}
+					else {
+						MessageBox.Show("You must agree to the terms and conditions \nto create a PatronAccount for the Rock Climbing wall.");
+					}
 				}
 			}
 			else {
-				MessageBox.Show("You must agree to the terms and conditions \nto create a PatronAccount for the Rock Climbing wall.");
+				MessageBox.Show("Make sure all textboxes are filled out!");
 			}
 		}
 	}
