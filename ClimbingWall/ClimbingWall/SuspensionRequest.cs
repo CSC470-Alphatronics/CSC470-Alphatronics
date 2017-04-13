1933749afe6e270516948494fa4646477e070388
Michael Bailey <michael.bailey258@jacks.sdstate.edu>
2017-04-13 04:23:15 -0500

    Added more input validation

--- ClimbingWall/ClimbingWall/SuspensionRequest.cs
+++ ClimbingWall/ClimbingWall/SuspensionRequest.cs
@@ -19,10 +19,20 @@ namespace ClimbingWall
 
 		private void reqButton_Click(object sender, EventArgs e)
 		{
-			//Patron ID validation goes here
-
-			String reqText = "Suspension Request for Patron: " + idTextBox.Text + " For: " + reasonTextBox.Text + ". Suggested Suspension: " + lengthTextBox.Text;
-			Database.Instance.addNote(reqText, 10);
+            //Patron ID validation goes here
+            if (!DatabaseInputValidation.uintIsValidWithinRange(idTextBox.Text, 0, 9999999))
+            {
+                MessageBox.Show("Please enter a valid patron ID!");
+            }
+            if (!String.IsNullOrEmpty(idTextBox.Text)
+                && DatabaseInputValidation.mandatoryStringIsValid(reasonTextBox.Text, 1) && DatabaseInputValidation.mandatoryStringIsValid(lengthTextBox.Text, 1)){
+                String reqText = "Suspension Request for Patron: " + idTextBox.Text + " For: " + reasonTextBox.Text + ". Suggested Suspension: " + lengthTextBox.Text;
+                Database.Instance.addNote(reqText, 10);
+            }
+            else
+            {
+                MessageBox.Show("Please enter information for all fields!");
+            }
 
 		}
 	}
